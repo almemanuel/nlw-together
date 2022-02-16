6fcf51bf71780475197d51dda0ce17b3a278de47
emanuel <almemanuel>
2022-02-16 13:02:16 -0300

    server.js 4st lecture view-engine config

--- src/server.js
+++ src/server.js
@@ -6,11 +6,8 @@ const server = express()
 
 server.set('view engine', 'ejs')
 
-server.use(express.static('public'))
-
 server.set('views', path.join(__dirname, 'views'))
 
 server.use(route)
 
 server.listen(3000, () => console.log("RUN"))
-
