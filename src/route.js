7a3191126be28464f7ed979e987301a36e7cefb2
emanuel <almemanuel>
2022-02-16 13:20:16 -0300

    route.js 4st lecture

--- src/route.js
+++ src/route.js
@@ -4,6 +4,6 @@ const route = express.Router()
 
 route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/create-class', (req, res) => res.render("create-class"))
 
 module.exports = route
